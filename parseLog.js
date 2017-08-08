be2d5d212e0571e8528dc52863a228496843b332
Wei Zhang <turbozv@gmail.com>
2017-08-07 18:04:33 -0700

    Make it easier to export to csv

--- parseLog.js
+++ parseLog.js
@@ -1,7 +1,8 @@
 var sqlite3 = require('sqlite3-promise')
 
 async function init() {
-    let html = '';
+    let summary = '';
+    let details = '';
     var db = new sqlite3.Database('log.db');
     var result = await db.allAsync('SELECT COUNT(*) AS Count FROM logView');
     const totalCount = result[0].Count;
@@ -11,30 +12,70 @@ async function init() {
     const totalSessions = result[0].Count;
     result = await db.allAsync("SELECT SUM(cost) AS Count from logView");
     const totalCost = result[0].Count;
-    html += ("Total count: " + totalCount) + "<br>\n";
-    html += ("Total devices: " + totalDevices) + "<br>\n";
-    html += ("Total sessions: " + totalSessions) + "<br>\n";
-    html += ("Average response time: " + (totalCost / totalCount).toFixed(2) + "ms") + "<br>\n";
+    result = await db.allAsync('SELECT DATE(LocalDate) AS Date FROM logView ORDER BY LocalDate ASC LIMIT 1');
+    const startDate = result[0].Date;
+    result = await db.allAsync('SELECT DATE(LocalDate) AS Date FROM logView ORDER BY LocalDate DESC LIMIT 1');
+    const endDate = result[0].Date;
+    summary += ("Start date, " + startDate) + "\n";
+    summary += ("End date, " + endDate) + "\n";
+    summary += ("Total requests, " + totalCount) + "\n";
+    summary += ("Total unique devices, " + totalDevices) + "\n";
+    summary += ("Total sessions, " + totalSessions) + "\n";
+    summary += ("Average response time, " + (totalCost / totalCount).toFixed(2) + "ms") + "\n";
 
-    result = await db.allAsync("SELECT DISTINCT DATE(LocalDate) AS Days from logView ORDER BY Days DESC");
-    resultByDays = [];
-    for (var i in result) {
-        const date = result[i].Days;
-        html += ("-- " + date + " --") + "<br>\n";
-        resultDayCount = await db.allAsync("SELECT COUNT(*) AS Count from logView WHERE DATE(LocalDate)='" + date + "'");
-        resultDayDevices = await db.allAsync("SELECT COUNT(DISTINCT deviceId) AS Count from logView WHERE deviceId<>'' AND DATE(LocalDate)='" + date + "'");
-        resultDaySessions = await db.allAsync("SELECT COUNT(DISTINCT sessionId) AS Count from logView WHERE sessionId<>'' AND DATE(LocalDate)='" + date + "'");
-        resultByDays.push({
-            Count: resultDayCount[0].Count,
-            Devices: resultDayDevices[0].Count,
-            Sessions: resultDaySessions[0].Count
-        });
-        html += ("Count: " + resultDayCount[0].Count) + "<br>\n";
-        html += ("Devices: " + resultDayDevices[0].Count) + "<br>\n";
-        html += ("Sessions: " + resultDaySessions[0].Count) + "<br>\n";
+    details += "Date, Requests, Unique devices, Sessions\n";
+    var start = new Date(startDate);
+    var end = new Date(endDate);
+    while (start <= end) {
+        date = start.toISOString().substring(0, 10);
+        resultByDays = [];
+        for (var i in result) {
+            resultDayCount = await db.allAsync("SELECT COUNT(*) AS Count from logView WHERE DATE(LocalDate)='" + date + "'");
+            resultDayDevices = await db.allAsync("SELECT COUNT(DISTINCT deviceId) AS Count from logView WHERE deviceId<>'' AND DATE(LocalDate)='" + date + "'");
+            resultDaySessions = await db.allAsync("SELECT COUNT(DISTINCT sessionId) AS Count from logView WHERE sessionId<>'' AND DATE(LocalDate)='" + date + "'");
+            resultByDays.push({
+                Count: resultDayCount[0].Count,
+                Devices: resultDayDevices[0].Count,
+                Sessions: resultDaySessions[0].Count
+            });
+            details += (date) + ", " + (resultDayCount[0].Count) + ", " + (resultDayDevices[0].Count) + ", " + (resultDaySessions[0].Count) + "\n";
+        }
+
+        start.setDate(start.getDate() + 1);
     }
 
-    console.log(html);
+    details += "\nDeviceId, Language, LastSeen, DaysSinceLastVisit\n";
+    result = await db.allAsync("SELECT DISTINCT deviceId from logView WHERE deviceId<>''");
+    devices = [];
+    langs = {};
+    const now = new Date();
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    for (var i in result) {
+        devices.push(result[i].deviceId);
+    }
+    for (var i in devices) {
+        const deviceId = devices[i];
+        result = await db.allAsync("SELECT lang, LocalDate FROM logView WHERE deviceId='" + deviceId + "' AND path='/lessons/' ORDER BY date DESC LIMIT 1");
+        const lang = result[0].lang;
+        const lastSeen = result[0].LocalDate;
+        var diffDays = Math.round(Math.abs((now.getTime() - (new Date(lastSeen)).getTime()) / oneDay));
+        details += (deviceId + ", " + lang + ', ' + lastSeen + ', ' + diffDays) + "\n";
+        if (langs[lang]) {
+            langs[lang]++;
+        } else {
+            langs[lang] = 1;
+        }
+    }
+
+    summary += "\nLanguage, DeviceCount, Percentage\n";
+    for (var i in langs) {
+        summary += (i + ', ' + langs[i] + ', ' + (langs[i] * 100 / totalDevices).toFixed(1) + '%') + "\n";
+    }
+
+    //console.log('===== Summary =====');
+    console.log(summary);
+    //console.log('===== Details =====');
+    console.log(details);
 }
 
 init();
