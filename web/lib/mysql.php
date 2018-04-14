1934bac0bd8b265c79be864048368b408da98128
Wei Zhang <turbozv@gmail.com>
2018-04-14 09:13:52 -0700

    Custom sort ordering for GL

--- web/lib/mysql.php
+++ web/lib/mysql.php
@@ -38,7 +38,8 @@ function getMembers($class, $group)
     if ($group == 0) {
         $result = getQuery("select id from users where class=$class and role NOT IN (6,255) order by role, cname asc");
     } else {
-        $result = getQuery("select id from users where class=$class and `group`=$group order by role, cname asc");
+        // 6 - GL shows first
+        $result = getQuery("select id from users where class=$class and `group`=$group order by FIELD(role, '6') desc, cname asc");
     }
     while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
         $members[$line['id']] = $g_users[$line['id']];
