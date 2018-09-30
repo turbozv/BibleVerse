608f6cd81f12d86a3f9272a2b8d79d78dd5b2dcd
Wei Zhang <turbozv@gmail.com>
2018-09-30 15:28:57 -0700

    Revert since server only supports nodejs6

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let mysql = require('mysql');
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const util = require('util');
 
 let dbBible = new sqlite3.Database('bible.db');
 let jsonParser = bodyParser.json()
@@ -15,8 +14,6 @@ let mysqlConn = mysql.createConnection({ host: config.mysqlServer, user: config.
 
 // Keep connection alive
 mysqlConn.connect();
-
-const mysqlQuery = util.promisify(mysqlConn.query).bind(mysqlConn);
 
 const ValidLanguages = ["chs", "cht", "eng", "spa"];
 const ValidBibleVersions = ['afr53', 'afr83', 'akjv', 'alab', 'amp', 'ampc', 'apsd', 'arc09', 'asv', 'avddv', 'bcnd', 'bdc', 'bdk', 'bds', 'bhn', 'bhti', 'bimk', 'bjb', 'bk', 'bl92', 'bm', 'bmdc', 'bpt', 'bpv', 'bysb', 'ccb', 'ceb', 'cev', 'cevd', 'cjb', 'cnvs', 'cnvt', 'csbs', 'cunpss', 'cunpts', 'darby', 'dhh', 'dnb1930', 'dra', 'erv', 'ervar', 'ervhi', 'ervmr', 'ervne', 'ervor', 'ervpa', 'ervta', 'ervur', 'esv', 'exb', 'fnvdc', 'gnt', 'gnv', 'gw', 'hau', 'hcsb', 'hcv', 'hhh', 'hlgn', 'hnzri', 'htb', 'icb', 'igbob', 'isv', 'jnt', 'jub', 'kj21', 'kjv', 'kpxnt', 'leb', 'lsg', 'maori', 'mbb05', 'mev', 'mounce', 'msg', 'n11bm', 'n78bm', 'nabre', 'nasb', 'natwi', 'nav', 'nbg51', 'nblh', 'ncv', 'neg1979', 'net', 'ngude', 'nirv', 'niv1984', 'niv2011', 'nivuk', 'nkjv', 'nlt', 'nlt2013', 'nlv', 'nog', 'nr2006', 'nrsv', 'nrsva', 'nrt', 'nso00', 'nso51', 'ntlr', 'ntv', 'nvi', 'nvipt', 'ojb', 'okyb', 'ondb', 'phillips', 'pmpv', 'pnpv', 'rcpv', 'rcuvss', 'rcuvts', 'ripv', 'rnksv', 'rsv', 'rsvce', 'rvc', 'rvr1995', 'rvr60', 'rvr95', 'rvv11', 'rwv', 'sblgnt', 'sch2000', 'seb', 'sg21', 'snd', 'snd12', 'spynt', 'sso89so', 'suv', 'swt', 'synod', 'tb', 'tbov', 'tcl02', 'th1971', 'tla', 'tlb', 'tlv', 'tr1550', 'tr1894', 'tso29no', 'tso89', 'tsw08no', 'tsw70', 'urd', 'ven98', 'voice', 'web', 'webbe', 'wlc', 'wyc', 'xho75', 'xho96', 'ylt', 'zomi', 'zul59'];
@@ -393,7 +390,7 @@ app.get('/attendance', function (req, res) {
 })
 
 // Get teaching audio
-app.get('/audio/*', async function (req, res) {
+app.get('/audio/*', function (req, res) {
   const client = getClientInfo(req);
   let logger = new Logger(req, client);
   const cellphone = req.params[0];
@@ -417,9 +414,11 @@ app.get('/audio/*', async function (req, res) {
     };
   }
 
-  try {
-    let result = await mysqlQuery(query);
-    if (result.length === 0) {
+  mysqlConn.query(query, function (error, result, fields) {
+    if (error) {
+      sendErrorObject(res, 400, { Error: JSON.stringify(error) });
+      logger.error(error);
+    } else if (result.length == 0) {
       sendErrorObject(res, 400, { Error: "Invalid user or no permission" });
       logger.error();
     } else {
@@ -434,10 +433,7 @@ app.get('/audio/*', async function (req, res) {
       }
       logger.succeed();
     }
-  } catch (error) {
-    sendErrorObject(res, 400, { Error: JSON.stringify(error) });
-    logger.error(error);
-  }
+  });
 })
 
 // Get user information
